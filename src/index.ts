436cc95fc7d06c5b9a424f5ddeb29ecc1649b3a2
Oleg Sozonik <sozonik999@gmail.com>
2023-07-24 11:46:05 +0300

    fix vercel config

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import express, {Request, Response} from 'express';
+const express = require('express');
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (req: any, res: any) => {
     debugger;
     let helloMessage = 'Hello incubator';
     res.send(helloMessage)
